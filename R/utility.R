f8193e8bf150b996583b5dfd1863d02b2e2bfe9c
Christopher Nobles <cnobles@upenn.edu>
2017-11-03 11:15:10 -0400

    Added alias_arbiter function and documentation.

--- R/utility.R
+++ R/utility.R
@@ -97,3 +97,117 @@ map_names <- function(data, names = "gene_name",
 
   return(data)
 }
+
+#' Assign consistant ID to variable given various alias IDs from a reference.
+#'
+#' @usage
+#'   alias_arbiter(IDs, RefIDs, aliasIDs)
+#'   alias_arbiter(
+#'     IDs, RefIDs, aliasIDs, outputIDs, sep = NULL,
+#'     remove_absent_IDs = FALSE, quiet = FALSE)
+#'
+#' @description Assign a reference ID to a vector of IDs that may have alias
+#' values. An example, if ID values of "A", "B", "C" were given to three
+#' bacterial species, but "B" was later reclassified as "C", then "B" becomes
+#' a depreciated alias of "C". This function would take this information and
+#' change the original vector of "A", "B", and "C", to "A", "C", and "C".
+#' Aliases can be supplied as a list of character or numeric vectors or as a
+#' character vector to split on the `sep` value. IDs need to match RefIDs, but
+#' outputIDs can be another ID type, as long as they are indexed in the same
+#' order as RefIDs. Any ambiguous aliases will be removed, but will be messaged
+#' to the output. Any IDs not matching RefIDs or aliasIDs will be returned as
+#' "NA", unless "NULL" is provided to the option `remove_absent_IDs`. For this
+#' option, the default value is `FALSE` which will return `NA` for unmatched IDs
+#' and `TRUE` will remove all `NA` values. Caution, this produces an output that
+#' is not the same length as the input if there are any unmatched or ambiguous
+#' IDs. The other option is `NULL` to be supplied, which fills the `NA` values
+#' with their original input, even if the alias was ambiguous.
+#'
+#' This function created a directed graph where all reference IDs are source
+#' nodes and all aliases are children from that source node. Alias IDs are
+#' screened for reference IDs and ambiguous IDs first, therefore ensuring the
+#' graph is a collection of small subgraphs with single sources and multiple
+#' sinks.
+#'
+#' @param IDs character vector of IDs which need to be consistantly assigned.
+#' Must be same type as RefIDs.
+#' @param RefIDs character vector of IDs for reference, must be the same length
+#' and order as aliasIDs and, if included, outputIDs.
+#' @param aliasIDs list of character vectors giving aliases for given RefIDs, or
+#' character vector to be split for aliases (must specify the `sep` argument
+#' with delimiter).
+#' @param outputIDs character or numeric vector for output. Indexed in the same
+#' order as the RefIDs and aliasIDs. If not given, RefIDs will be used as
+#' the output IDs
+#' @param sep character delimiter for aliasIDs if supplied in a character
+#' vector. Default is `NULL`.
+#' @param remove_absent_IDs logical. Default behavior will change IDs not found
+#' in RefIDs or aliases to `NA`. If set to TRUE, IDs not matching RefIDs
+#' or aliasIDs will be removed. Alternatively, if set to NULL, IDs not matching
+#' RefIDs or aliasIDs will remain as input ID values.
+#' @param quiet logical. If TRUE, message output will be silenced. Messages
+#' include notification of ambiguous aliases or aliases which belong to two or
+#' more reference IDs.
+#'
+#' @example
+#' # Using letters as IDs
+#' IDs <- LETTERS[1:10]
+#' RefIDs <- c("A", "C", "E", "J")
+#' aliasIDs <- c("A,B", "C,D", "E,F,G,H,I", "J")
+#' alias_arbiter(IDs, RefIDs, aliasIDs, sep = ",")
+#'
+#' @author Christopher Nobles, Ph.D.
+#' @export
+
+alias_arbiter <- function(IDs, RefIDs, aliasIDs, outputIDs = NULL,
+                          sep = NULL, remove_absent_IDs = FALSE, quiet = FALSE){
+  # Require packages
+  stopifnot(require("igraph"))
+
+  # Check inputs and assign defaults
+  stopifnot(any(IDs %in% RefIDs))
+  stopifnot(length(RefIDs) == length(aliasIDs))
+  if(!is.null(outputIDs)){
+    stopifnot(length(outputIDs) == length(RefIDs))
+  }else{
+    outputIDs <- RefIDs
+  }
+  if(class(aliasIDs) != "list"){
+    stopifnot(any(sapply(aliasIDs, function(x) grepl(sep, x))))
+    aliasIDs <- strsplit(aliasIDs, sep, fixed = TRUE)
+  }
+
+  # Check for ambiguous aliases
+  ambi_alias <- table(unlist(aliasIDs))
+  ambi_alias <- names(ambi_alias[ambi_alias > 1])
+  if(length(ambi_alias) > 0){
+    message(
+      "Ambiguous aliases removed : ", paste(ambi_alias, collapse = ", "), ".")
+  }
+
+  # Remove RefIDs and ambiguous aliases from aliasIDs
+  aliasIDs <- lapply(1:length(aliasIDs), function(i){
+    aliasIDs[[i]][!aliasIDs[[i]] %in% c(RefIDs[i], ambi_alias)]
+  })
+
+  # Create graph of aliases and reference IDs
+  E <- data.frame(
+    vi = rep(RefIDs, sapply(aliasIDs, length)),
+    vj = unlist(aliasIDs))
+  V <- data.frame(v = unique(c(RefIDs, unlist(aliasIDs))))
+  G <- construct_graph(E, V, mode = "directed")
+
+  # Assign IDs to clusters (maybe this should be sources instead of clusters)
+  mem <- igraph::clusters(G)$membership
+  IDmem <- mem[IDs]
+  Refmem <- mem[RefIDs]
+
+  # Assign output IDs to input IDs and return
+  outIDs <- outputIDs[match(IDmem, Refmem)]
+  if(is.null(remove_absent_IDs)){
+    outIDs <- ifelse(is.na(outIDs), IDs, outIDs)
+  }else if(remove_absent_IDs == TRUE){
+    outIDs <- outIDs[!is.na(outIDs)]
+  }
+  return(outIDs)
+}
